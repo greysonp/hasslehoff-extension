b421fb5dfdda851d3d5c65b1f74bb29efef51e84
Greyson Parrelli <greyson.parrelli@gmail.com>
2015-08-06 01:12:18 -0700

    Initial commit.

---
+++ js/contentscript.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+    $('img').each(function() {
+        if (Math.random() < 0.05) {
+            $(this).attr('src', 'https://pbs.twimg.com/profile_images/475692665540182016/BAmdIaM8.jpeg');
+        }
+    });
+});
